65087904cc39e8637bf5f12e22e4dcc207282fd6
TatarchukKO <dolbyone007@gmail.com>
2018-01-30 19:33:34 +0300

    first lesson: article

---
+++ src/article.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Component } from 'react'
+
+class Article extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      isOpen: false,
+    }
+
+    this.toggleOpen = this.toggleOpen.bind(this);
+  }
+
+  toggleOpen() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    })
+  }
+
+  getBody() {
+    if (!this.state.isOpen) {
+      return null
+    }
+
+    const { article } = this.props
+
+    return <section>{article.text}</section>
+  }
+
+  render() {
+    const { article } = this.props
+    const { isOpen } = this.state
+
+    return (
+      <div>
+        <h3>{article.title}</h3>
+        <button onClick={this.toggleOpen}>
+          {isOpen ? 'close' : 'open'}
+        </button>
+        {this.getBody()}
+      </div>
+    )
+  }
+}
+
+export default Article
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+import Article from './article'
+import { articles } from './fixtures'
+
+function HelloWorld() {
+  return <h1>Hello World</h1>
+}
+
+render(<Article article={articles[0]} />, document.getElementById('container'))
